27f6499c54314e96cbb3de88e0793065bd7d255b
laksmi santo <84493741+laksmisanto@users.noreply.github.com>
2023-08-21 00:51:44 +0600

    footer and copyright section update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,91 +18,106 @@ const montserrat = Montserrat({
 const Footer = () => {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-3">
-            <div className="short__info__footer">
-              <Link href="/">
-                <Image src={Logo} loading="lazy" alt="Footer-Logo" />
-              </Link>
-              <div className="footer__info__text">
-                <p className={montserrat.className}>
-                  There are many variations of passages of available, but the
-                  majority have suffered.
-                </p>
+      <footer className="footer__section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3">
+              <div className="short__info__footer">
+                <Link href="/">
+                  <Image src={Logo} loading="lazy" alt="Footer-Logo" />
+                </Link>
+                <div className="footer__info__text">
+                  <p className={montserrat.className}>
+                    There are many variations of passages of available, but the
+                    majority have suffered.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="footer__service">
-              <div className="footer__service__title">
-                <h3 className={montserrat.className}>Services</h3>
+            <div className="col-lg-3">
+              <div className="footer__service">
+                <div className="footer__service__title">
+                  <h3 className={montserrat.className}>Services</h3>
+                </div>
+                <div className="footer__service__menu">
+                  {footerServiceData.map((item, i) => (
+                    <Link
+                      className={montserrat.className}
+                      href={item.link}
+                      key={i}
+                    >
+                      {item.title}
+                    </Link>
+                  ))}
+                </div>
               </div>
-              <div className="footer__service__menu">
-                {footerServiceData.map((item, i) => (
-                  <Link
+            </div>
+            <div className="col-lg-3">
+              <div className="footer__resource">
+                <div className="footer__resource__title">
+                  <h3 className={montserrat.className}>Resources</h3>
+                </div>
+                <div className="footer__resource__menu">
+                  {footerResourceData.map((item, i) => (
+                    <Link
+                      className={montserrat.className}
+                      href={item.link}
+                      key={i}
+                    >
+                      {item.title}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <div className="footer__newslatter">
+                <div className="footer__newslatter__title">
+                  <h3 className={montserrat.className}>Newsletter</h3>
+                </div>
+                <div className="footer__newslatter__text">
+                  <p className={montserrat.className}>
+                    Subscribe for our upcoming latest address and resources
+                  </p>
+                </div>
+                <div className="footer__newslatter__form">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
                     className={montserrat.className}
-                    href={item.link}
-                    key={i}
-                  >
-                    {item.title}
-                  </Link>
-                ))}
+                  />
+                  <button>
+                    <BsSend />
+                  </button>
+                </div>
+                <div className="footer__social__link">
+                  <div className="social__icon">
+                    <Facebook />
+                  </div>
+                  <div className="social__icon">
+                    <Instagram />
+                  </div>
+                  <div className="social__icon">
+                    <Twitter />
+                  </div>
+                  <div className="social__icon">
+                    <Pintarest />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-lg-3">
-            <div className="footer__resource">
-              <div className="footer__resource__title">
-                <h3 className={montserrat.className}>Resources</h3>
-              </div>
-              <div className="footer__resource__menu">
-                {footerResourceData.map((item, i) => (
-                  <Link
-                    className={montserrat.className}
-                    href={item.link}
-                    key={i}
-                  >
-                    {item.title}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="footer__newslatter">
-              <div className="footer__newslatter__title">
-                <h3 className={montserrat.className}>Newsletter</h3>
-              </div>
-              <div className="footer__newslatter__text">
+        </div>
+      </footer>
+      <div className="copyright__section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 mx-auto text-center">
+              <div className="copyright__text">
                 <p className={montserrat.className}>
-                  Subscribe for our upcoming latest address and resources
+                  &copy;copyright 2023 || laksmi santo
                 </p>
-              </div>
-              <div className="footer__newslatter__form">
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  className={montserrat.className}
-                />
-                <button>
-                  <BsSend />
-                </button>
-              </div>
-              <div className="footer__social__link">
-                <div className="social__icon">
-                  <Facebook />
-                </div>
-                <div className="social__icon">
-                  <Instagram />
-                </div>
-                <div className="social__icon">
-                  <Twitter />
-                </div>
-                <div className="social__icon">
-                  <Pintarest />
-                </div>
               </div>
             </div>
           </div>
